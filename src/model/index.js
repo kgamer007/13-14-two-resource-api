0fe1c1ad6861141b149345f60cb38b3cf6ff6887
Kevin Hwangpo <hwangpo3@gmail.com>
2018-07-05 11:10:36 -0700

    adding model

---
+++ src/model/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import util from 'util';
+import fs from 'fs';
+
+const readDirectoryPromise = util.promisify(fs.readdir);
+const modelPath = `${__dirname}`;
+
+export default () => {
+  return readDirectoryPromise(modelPath)
+    .then((files) => {
+      const newFiles = files.filter(file => file !== 'index.js').map(file => `./${file}`);
+      const modelMap = newFiles.reduce((storage, currentFile) => {
+        const file = require(currentFile) /*eslint-disable-line*/
+        const isMongooseModel = file.default && file.default.modelName;
+        const modelName = isMongooseModel ? file.default.modelName : currentFile;
+        storage[modelName] = file;
+        return storage;
+      }, {});
+   
+      return modelMap;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
